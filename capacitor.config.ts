1ba6dd631bf4cdaf89d50707321f4decf3eef4e5
felipegc79 <luisfelipegc79@gmail.com>
2026-05-17 13:41:58 -0500

    Initialize and configure Capacitor mobile architecture (Android & iOS) with custom build scripts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tikkaconsultores.gestionpyp',
+  appName: 'Tikka Gestion PyP',
+  webDir: 'build'
+};
+
+export default config;
